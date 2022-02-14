cae42c4b5e424074968c512ab25901272f6522c6
Zhandos <200103398@stu.sdu.edu.kz>
2022-02-15 00:00:01 +0600

    Created

---
+++ app/src/main/java/com/baribir/sdugram/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.baribir.sdugram
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNav_view)
+        val navController = findNavController(R.id.navHostFragment)
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
